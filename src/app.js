151ebfc9268f8f61b93571af02619d5894645c1b
Nischal <nischalnewar009@gmail.com>
2023-01-12 09:51:14 -0500

    changed the port number

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('../src/utils/forecastCode');
 const hbs = require('hbs');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 //set paths
 const staticPath = path.join(__dirname,"../public");
